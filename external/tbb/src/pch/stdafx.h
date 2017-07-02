1c8da576a643f4a5e9bd5eafa48bf15a79fc3805
Anton Filimonov <anton.filimonov@gmail.com>
2017-07-02 16:32:32 +0300

    Fix tbb pch for static lib

---
+++ external/tbb/src/pch/stdafx.h
@@ -0,0 +1,12 @@
+#pragma GCC system_header
+#ifdef __cplusplus
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <list>
+#include <deque>
+#include <memory>
+#include <string>
+#endif
