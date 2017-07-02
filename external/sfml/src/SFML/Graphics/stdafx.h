5cb0822d0fa4b423792d61130db45caa37a3be1b
Anton Filimonov <anton.filimonov@gmail.com>
2017-07-02 16:20:23 +0300

    Fix pch for windows for sfml

---
+++ external/sfml/src/SFML/Graphics/stdafx.h
@@ -0,0 +1,12 @@
+#pragma GCC system_header
+#ifdef __cplusplus
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <set>
+#include <stack>
+#endif
