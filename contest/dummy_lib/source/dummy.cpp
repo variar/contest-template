2074a4b0ba1e4f29d6355f5d5af8a8ed3f38bd86
Anton Filimonov <anton.filimonov@gmail.com>
2017-07-05 02:31:10 +0300

    Remove pplx. Allow to build shared tbb

--- contest/dummy_lib/source/dummy.cpp
+++ contest/dummy_lib/source/dummy.cpp
@@ -2,11 +2,8 @@
 
 #include <boost/any.hpp>
 
-#include <pplx/pplxtasks.h>
 #include <iostream>
 
 Dummy::Dummy() 
 {
-    auto task = pplx::create_task([](){ std::cout << "PPL task" << std::endl;});
-    task.get();
 }
